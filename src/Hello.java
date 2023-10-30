279c4c0f3f356e782987fa6f4037b8d2e4703f94
Illia Tkachov <i.o.tkachov@gmail.com>
2023-10-30 21:59:36 +0200

    make addition calculator

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Hello {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
     }
 }
