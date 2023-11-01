ca1a125d084a3f32450e2bc7984142b4bd3aba69
Illia Tkachov <i.o.tkachov@gmail.com>
2023-11-01 18:07:23 +0200

    1

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Hello {
 
     public static void main(String[] args) {
+        // haha comment
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
