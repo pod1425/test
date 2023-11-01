d4355c49f7484f2367047d22dae6670397947063
Illya <i.o.tkachov@gmail.com>
2023-11-01 18:07:06 +0200

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // haha comment
+        // one more comment
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
