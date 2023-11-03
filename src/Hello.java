582efe0f8b9b79873f11c2a3e32e05946bed3992
Illia Tkachov <i.o.tkachov@gmail.com>
2023-11-03 20:10:56 +0200

    Add readme and gitignore

--- src/Hello.java
+++ src/Hello.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 public class Hello {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a + b);
+        System.out.println("Hello");
     }
 }
